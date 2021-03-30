5afad56fe9bd0a28d63ae5d6bbaec033afa840a3
karthi <karthi@exm.com>
2021-03-30 12:35:31 +0530

    around mixins

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,7 +1,14 @@
 import 'dart:async';
+import 'validator.dart';
 
-class Bloc {
-  final emailContoller = StreamController<String>();
-  final passwordController = StreamController<String>();
-} 
+class Bloc extends Object with Validators {
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
+
+  Stream<String> get email => _email.stream;
+  Stream<String> get password => _password.stream;
+
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
+}
 
